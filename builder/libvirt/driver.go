5b45c76beacfe2c3b4a1dfd8ef974041d8ee43ba
Mathieu Tortuyaux <mathieu.tortuyaux@gmail.com>
2020-05-03 19:21:10 +0200

    driver: add driver interface
    this interface will be used mainly for tests but also to be able
    to implement different libvirt variant following provider

---
+++ builder/libvirt/driver.go
@@ -0,0 +1,24 @@
+package libvirt
+
+// Driver is the interface
+// that has to be implemented to communicate
+// in order to communicate with Libvirt host
+type Driver interface {
+	// GetPool returns a pool matching the string
+	// this prevents duplication error
+	GetPool(string) (Pool, error)
+
+	// CreatePool creates a pool on the libvirt
+	// host
+	CreatePool(Pool) (Pool, error)
+}
+
+type Pool interface {
+	// GetName returns the name of
+	// the pool
+	GetName() (string, error)
+
+	// GetXML returns the XML template
+	// rendered with the actual values
+	GetXML() (string, error)
+}
